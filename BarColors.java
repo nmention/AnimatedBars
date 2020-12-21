f7e696e310188bcc538b3270ae701d64fa011a8d
nmention <noe.mention@protonmail.com>
2020-12-21 19:58:44 +0100

    #5 Add color list BarColors

---
+++ BarColors.java
@@ -0,0 +1,16 @@
+import java.awt.*;
+
+public class BarColors {
+    public static Color[] colors = {
+            new Color(66, 215, 212),
+            new Color(61, 110, 158),
+            new Color(138, 29, 150),
+            new Color(150, 3, 5),
+            new Color(255, 136, 0),
+            new Color(238, 230, 7),
+            new Color(43, 107, 11),
+            new Color(35, 63, 19),
+            new Color(29, 42, 37),
+            new Color(5, 5, 5),
+    };
+}
